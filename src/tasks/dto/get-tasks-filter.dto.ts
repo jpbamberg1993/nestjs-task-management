b68c099b020ad67061f57e695ffaba3671b63e70
J. Paul Bamberg <jpbamberg1993@gmail.com>
2020-04-26 12:34:35 -0400

    Validate incoming filter request

--- src/tasks/dto/get-tasks-filter.dto.ts
+++ src/tasks/dto/get-tasks-filter.dto.ts
@@ -1,6 +1,12 @@
 import { TaskStatus } from '../task.model';
+import { IsIn, IsNotEmpty, IsOptional } from 'class-validator';
 
 export class GetTasksFilterDto {
+  @IsOptional()
+  @IsIn([TaskStatus.IN_PROGRESS, TaskStatus.DONE, TaskStatus.OPEN])
   status: TaskStatus;
+
+  @IsOptional()
+  @IsNotEmpty()
   search: string;
 }
