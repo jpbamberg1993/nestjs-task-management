1781767e6e9cac3e407c57ed0d67dd19e326db50
J. Paul Bamberg <jpbamberg1993@gmail.com>
2020-04-27 15:03:08 -0400

    Create basic sign up functionality

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,15 @@
+import { Body, Controller, Post, Query, ValidationPipe } from '@nestjs/common';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import { AuthService } from './auth.service';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('sign-up')
+  signUp(
+    @Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto,
+  ): Promise<void> {
+    return this.authService.signUp(authCredentialsDto);
+  }
+}
