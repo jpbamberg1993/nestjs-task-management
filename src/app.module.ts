1ad6208cdf58728f02d8e7b05d5e6c8c442ba102
J. Paul Bamberg <jpbamberg1993@gmail.com>
2020-05-01 10:07:41 -0400

    Change order modules are required

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,6 @@ import { typeOrmConfig } from './config/typeorm.config';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [TasksModule, TypeOrmModule.forRoot(typeOrmConfig), AuthModule],
+  imports: [TypeOrmModule.forRoot(typeOrmConfig), TasksModule, AuthModule],
 })
 export class AppModule {}
