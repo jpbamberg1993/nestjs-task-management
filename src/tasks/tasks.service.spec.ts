c604f547129905137b1e59dde648a59ac666bb34
J. Paul Bamberg <jpbamberg1993@gmail.com>
2020-05-03 15:21:51 -0400

    Test TasksService.getTasks

---
+++ src/tasks/tasks.service.spec.ts
@@ -0,0 +1,40 @@
+import { Test } from '@nestjs/testing';
+import { TasksService } from './tasks.service';
+import { TaskRepository } from './task.repository';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
+import { TaskStatus } from './task-status.enum';
+
+const mockUser = { username: 'bamers' };
+
+const mocTaskRepository = () => ({
+  getTasks: jest.fn(),
+});
+
+describe('TaskService', () => {
+  let tasksService;
+  let taskRepository;
+
+  beforeEach(async () => {
+    const module = await Test.createTestingModule({
+      providers: [
+        TasksService,
+        { provide: TaskRepository, useFactory: mocTaskRepository }
+      ]
+    })
+    .compile();
+    tasksService = await module.get<TasksService>(TasksService);
+    taskRepository = await module.get<TaskRepository>(TaskRepository);
+  });
+
+  describe('getTasks', () => {
+    it('gets all tasks from repository', async () => {
+      taskRepository.getTasks.mockResolvedValue('somevalue');
+
+      expect(taskRepository.getTasks).not.toHaveBeenCalled();
+      const filters: GetTasksFilterDto = { status: TaskStatus.IN_PROGRESS, search: 'Some search query' };
+      const result = await tasksService.getTasks(filters, mockUser);
+      expect(taskRepository.getTasks).toHaveBeenCalled();
+      expect(result).toEqual('somevalue');
+    });
+  });
+});
